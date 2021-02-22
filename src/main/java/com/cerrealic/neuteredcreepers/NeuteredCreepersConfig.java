96914a06bfb1f90d7254c93919cb724cb126bd42
Stan <8138029+squiig@users.noreply.github.com>
2021-02-22 14:32:40 +0100

    Added config bools for about every exploding entity

--- src/main/java/com/cerrealic/neuteredcreepers/NeuteredCreepersConfig.java
+++ src/main/java/com/cerrealic/neuteredcreepers/NeuteredCreepersConfig.java
@@ -9,6 +9,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashSet;
 
 public class NeuteredCreepersConfig extends CerspiPluginConfig {
+	private final ConfigNode<Boolean> creeperGriefing = new ConfigNode<>("creeper-griefing", false);
+	private final ConfigNode<Boolean> crystalGriefing = new ConfigNode<>("crystal-griefing", true);
+	private final ConfigNode<Boolean> fireballGriefing = new ConfigNode<>("fireball-griefing", true);
+	private final ConfigNode<Boolean> tntMinecartGriefing = new ConfigNode<>("tnt-minecart-griefing", true);
+	private final ConfigNode<Boolean> tntGriefing = new ConfigNode<>("tnt-griefing", true);
+	private final ConfigNode<Boolean> witherGriefing = new ConfigNode<>("wither-griefing", true);
 
 	public NeuteredCreepersConfig(JavaPlugin plugin, FileConfiguration fileConfiguration) {
 		super(plugin, fileConfiguration);
@@ -16,6 +22,54 @@ public class NeuteredCreepersConfig extends CerspiPluginConfig {
 
 	@Override
 	protected HashSet<ConfigNode> getDefinedNodes() {
-		return Sets.newHashSet();
+		return Sets.newHashSet(creeperGriefing, crystalGriefing, fireballGriefing, tntMinecartGriefing, tntGriefing, witherGriefing);
+	}
+
+	public boolean getCreeperGriefing() {
+		return creeperGriefing.getValue();
+	}
+
+	public void setCreeperGriefing(boolean creeperGriefing) {
+		setNodeValue(this.creeperGriefing, creeperGriefing);
+	}
+
+	public boolean getCrystalGriefing() {
+		return crystalGriefing.getValue();
+	}
+
+	public void setCrystalGriefing(boolean crystalGriefing) {
+		setNodeValue(this.crystalGriefing, crystalGriefing);
+	}
+
+	public boolean getFireballGriefing() {
+		return fireballGriefing.getValue();
+	}
+
+	public void setFireballGriefing(boolean fireballGriefing) {
+		setNodeValue(this.fireballGriefing, fireballGriefing);
+	}
+
+	public boolean getTntMinecartGriefing() {
+		return tntMinecartGriefing.getValue();
+	}
+
+	public void setTntMinecartGriefing(boolean tntMinecartGriefing) {
+		setNodeValue(this.tntMinecartGriefing, tntMinecartGriefing);
+	}
+
+	public boolean getTntGriefing() {
+		return tntGriefing.getValue();
+	}
+
+	public void setTntGriefing(boolean tntGriefing) {
+		setNodeValue(this.tntGriefing, tntGriefing);
+	}
+
+	public boolean getWitherGriefing() {
+		return witherGriefing.getValue();
+	}
+
+	public void setWitherGriefing(boolean witherGriefing) {
+		setNodeValue(this.witherGriefing, witherGriefing);
 	}
 }
