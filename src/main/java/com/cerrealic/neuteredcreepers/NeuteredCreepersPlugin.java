038d87a9468880d3ed69af2ff591f929abdb9bf9
Stan <8138029+squiig@users.noreply.github.com>
2021-04-25 17:46:12 +0200

    Added resource id for auto updates

--- src/main/java/com/cerrealic/neuteredcreepers/NeuteredCreepersPlugin.java
+++ src/main/java/com/cerrealic/neuteredcreepers/NeuteredCreepersPlugin.java
@@ -5,10 +5,16 @@ import com.cerrealic.cerspilib.CerspiPlugin;
 import com.cerrealic.cerspilib.config.CerspiPluginConfig;
 
 public class NeuteredCreepersPlugin extends CerspiPlugin {
+	public final static int RESOURCE_ID = 89820;
 	private NeuteredCreepersConfig config;
 
 	public static NeuteredCreepersPlugin getInstance() {
 		return NeuteredCreepersPlugin.getPlugin(NeuteredCreepersPlugin.class);
+	}
+
+	@Override
+	public Integer getResourceId() {
+		return RESOURCE_ID;
 	}
 
 	public NeuteredCreepersConfig getNeuteredCreepersConfig() {
